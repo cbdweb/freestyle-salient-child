fb48fbc779cb7cf37066ab801664ad05c3385c0e
cbdweb <nik@cbdweb.net>
2014-07-29 17:02:19 +1000

    move signature to plugin;

--- js/freestyle.js
+++
@@ -1,8 +0,0 @@
-jQuery(document).ready(function($){
-	$('#old').html(
-		'<iframe style="background-color: #777; border: none;" src="http://domino.cbdweb.net/freestyle.nsf/indexwidget/infowidget?opendocument&' + 
-		(window.location.search ? window.location.search.substr(1) + "&" : "") + 
-		'referrer=' + encodeURI(document.referrer) + 
-		'" height="500" width="282" frameborder="0;">'
-	);
-})(jQuery)
