f087b53ffe9b34a2d63c709425ddeb7be16e7855
cbdweb <nik@cbdweb.net>
2015-10-10 11:02:16 +1100

    add sign-up block below all articles

--- functions.php
+++ functions.php
@@ -1,1 +1,10 @@
+<?php
+add_filter( 'the_content', 'sign_up' );
 
+function sign_up ( $content ) {
+    return $content .
+        "<div class='sign_up'>" . 
+        "<h3>Sign up to support helmet law reform</h3>" .
+        do_shortcode ( '[signature]' ) .
+        "</div>";
+}
