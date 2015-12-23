848d00d432b9a3b73392738653a5c59afa200b22
cbdweb <nik@cbdweb.net>
2015-12-24 08:23:16 +1100

    remove signature block from member signup pages

--- functions.php
+++ functions.php
@@ -2,9 +2,9 @@
 add_filter( 'the_content', 'sign_up' );
 
 function sign_up ( $content ) { 
-    if ( is_front_page() || get_the_title()==="Take Action"
+    if ( is_front_page() || get_the_title()==="Take Action" || get_the_title()==="Membership Levels" || get_the_title()==="Membership Checkout"
         ) return $content;
-    return $content .
+    return $content . get_the_title() . 
         "<div class='sign_up'>" . 
         "<h3>Sign up to support helmet law reform</h3>" .
         do_shortcode ( '[signature narrow="1"]' ) .
