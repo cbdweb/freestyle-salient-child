16e1e5f57b0e53fefc6d3b9ac75cddff02afabac
cbdweb <nik@cbdweb.net>
2021-02-17 20:14:33 +1100

    write own captcha;

--- functions.php
+++ functions.php
@@ -61,7 +61,7 @@ function fs_theme_sign () {
                             <option value="<?=$ab;?>"><?php echo $title;?></option>
                         <?php } ?>
                     </select></td></tr>
-            <tr><td class="leftcol">2+3=</td><td class="rightcol"><td class="rightcol"><input<?=($narrow || $popup) ? " class='smallinput'" : "";?> type="text" name="arithmetic" id="name<?=($popup ? "_popup" : "");?>"></td></tr>
+            <tr><td class="leftcol">2+3=</td><td class="rightcol"><input<?=($narrow || $popup) ? " class='smallinput'" : "";?> type="text" name="arithmetic" id="name<?=($popup ? "_popup" : "");?>"></td></tr>
             <tr><td class="leftcol">Comment:</td><td class="rightcol"><textarea name="excerpt" class="inputc"></textarea><br><div class="smallfont">Comments are subject to moderation</div></td></tr>
             <tr><td colspan="2"><button type="button" id="saveButton<?=($popup ? "_popup" : "");?>">Save</button></td></tr>
             <tr><td colspan="2"><div id="ajax-loading<?=($popup ? "_popup" : "");?>" class="farleft"><img src="<?php echo get_site_url();?>/wp-includes/js/thickbox/loadingAnimation.gif"></div></td></tr>
